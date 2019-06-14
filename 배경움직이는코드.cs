db9a854a46bd6f933db5bf79fe221a5f800a543d
SexyMg <hmg6435573@gmail.com>
2019-06-15 07:36:38 +0900

    Create 배경움직이는코드.cs

---
+++ 배경움직이는코드.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BGmove : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float startposition;
+    public float endPosition;
+    void Update()
+    {
+        // x포지션을 조금씩 이동
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        // 목표 지점에 도달했다면
+        if(speed<0)
+        {
+            if (transform.position.x <= endPosition)
+                transform.position = new Vector2(startposition, transform.position.y);///
+        }
+        else
+        {
+            if (transform.position.x >= endPosition)
+                transform.position = new Vector2(startposition, transform.position.y);///
+        }
+        
+    }
+}
