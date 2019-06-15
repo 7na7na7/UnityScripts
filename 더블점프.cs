c87a43b33180e4456d8a5e27f34acce1a0abc213
SexyMg <hmg6435573@gmail.com>
2019-06-15 15:52:04 +0900

    6_15 인디겜대회준비1일차

---
+++ 더블점프.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class 더블점프: MonoBehaviour
+{
+    public int jumpcount = 2;
+    public float jumpPower = 1f;
+    public Rigidbody2D rigid;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpcount > 0)
+            {
+                rigid.velocity = Vector2.zero;
+                Vector2 jumpVelocity = new Vector2(0, jumpPower);
+                rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
+                jumpcount--;
+            }
+        }
+    }
+
+    IEnumerator OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("ground")) //땅 닿을시 점프카운트 초기화
+        {
+            jumpcount = 2;
+        }
+    }
+}
---
+++ 생성_삭제.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class 생성_삭제: MonoBehaviour
+{
+    public Gameobject image; //생성할 오브젝트 받음
+    private GameObject obj;
+    
+    obj =  (GameObject)Instantiate(image); //이걸로 생성
+    
+    Destroy(obj.gameObject);//이걸로 삭제
+}
