2bdf003181714515959608069df70e3489037db2
SexyMg <hmg6435573@gmail.com>
2019-06-17 11:34:08 +0900

    버튼이미지 바꾸기코드
    이틀동안 못했는데 해냈다 ^^ 행복하다 ㅎㅎㅎㅎㅎㅎㅎㅎㅎ

---
+++ 버튼이미지바꾸기.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class 버튼이미지바꾸기 : MonoBehaviour
+{
+    public Sprite img; //여기 스프라이트 넣고
+    public Button btn; //여기 버튼 넣은 다음에
+    
+    public void changeimage () { //이걸로 버튼에 연결시켜서 Onclick()으로 실행시키면!
+        btn.gameObject.GetComponent<Image>().sprite = img;//이걸로 버튼의 이미지 컴포넌트를 얻어와 그것의 스프라이트를 pubic으로 넣은 img파일로 교체한다!
+        }
+}
