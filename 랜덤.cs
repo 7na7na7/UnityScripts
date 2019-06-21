d8a9474c92c8a0ca7c6c31eacbccf7f57d1911d7
SexyMg <hmg6435573@gmail.com>
2019-06-21 09:42:33 +0900

    Create 랜덤.cs

---
+++ 랜덤.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+//using UnityEngine.Random
+public class 랜덤: MonoBehaviour
+{
+    int random = Random.Range(0, 6); //random에 0에서 5까지의 값이 랜덤으로 저장됨
+}
