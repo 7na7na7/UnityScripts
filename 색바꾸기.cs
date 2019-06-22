adcf6edca9fe2e66fbf4bcdd901f109c096e769a
SexyMg <hmg6435573@gmail.com>
2019-06-22 22:08:43 +0900

    6_22일자 인디겜준비

---
+++ 색바꾸기.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+//using UnityEngine.Random
+public class 색바꾸기: MonoBehaviour
+{
+    public float colorchange=0.05f;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            normal();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            red();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            blue();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            green();
+        }
+
+    }
+    //0 normal
+    void normal() 
+    {
+        SpriteRenderer spr = GetComponent<SpriteRenderer>();
+        Color color = spr.color;
+        color.r += colorchange;
+        color.g += colorchange;
+        color.b += colorchange;
+        color.a += colorchange;
+        spr.color = color;
+    }
+
+//1 red
+    void red()
+    { 
+        SpriteRenderer spr = GetComponent<SpriteRenderer>();
+        Color color = spr.color;
+        if (color.r<255)
+        {
+            color.r += colorchange;
+            color.g = c;
+            color.b = c;
+            color.a += colorchange;
+            spr.color = color; 
+        }
+    }
+
+//2 blue
+    void blue()
+    {
+        SpriteRenderer spr = GetComponent<SpriteRenderer>();
+        Color color = spr.color;
+        if (color.b<255)
+        {
+            color.r = c;
+            color.g = c;
+            color.b +=colorchange;
+            color.a += colorchange;
+            spr.color = color;
+        }
+    }
+
+//3 green
+    void green()
+    {
+        SpriteRenderer spr = GetComponent<SpriteRenderer>();
+        Color color = spr.color;
+        if (color.g<255)
+        {
+            color.r = c;
+            color.g += colorchange;
+            color.b = c;
+            color.a += colorchange;
+            spr.color = color;
+        }
+    }
+}
---
+++ 캔버스생성.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class 캔버스생성: MonoBehaviour
+{
+    public Canvas homebtn;
+     public void pause () {
+        if(Time.timeScale==1)//정지 중이 아니면
+        {
+            Time.timeScale = 0;
+            obj = (Canvas) Instantiate(homebtn);
+        }
+        else if(Time.timeScale==0)//정지 중이면
+        {
+            Time.timeScale = 1;
+            Destroy(obj.gameObject);
+        }
+    }
+}
