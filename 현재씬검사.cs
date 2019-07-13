e7b1c10cd23a316412245b89f33555ddb5acac62
SexyM <hmg6435573@gmail.com>
2019-07-13 11:22:04 +0900

    현재 씬 검사함

---
+++ 현재씬검사.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class 현재씬검사 : MonoBehaviour
+{
+    private void Start()
+    {
+        if(SceneManager.GetActiveScene().name=="first")
+            Debug.Log("현재 씬은 first입니다.");
+    }
+}
