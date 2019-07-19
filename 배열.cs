87f849c044a5e975ebf12ebf09c4e06aca2bb44f
SexyM <hmg6435573@gmail.com>
2019-07-19 10:11:46 +0900

    Create 배열.cs

---
+++ 배열.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 배열 : MonoBehaviour
+{
+    int[] num = new int[5]; 
+
+    void Start()
+    {
+        num [0] = 1; 
+        num [1] = 2; 
+        num [2] = 3; 
+        num [3] = 4; 
+        num [4] = 5;
+    }
+    //위는 스크립트 내에서 배열에 할당하는 방식,
+    public int[] array; //그러나 이렇게 public 으로 배열을 할당받을 수도 있다.
+    
+}
