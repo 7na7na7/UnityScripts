924ae9c7b309bc4db06d4ab9b0a8e7045db53b6a
SexyM <hmg6435573@gmail.com>
2019-07-25 09:29:13 +0900

    AddForce
    rigidbody컴포넌트를 얻어와 힘을 가해 주는 코드이다.

---
+++ AddForce.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour
+{
+    private void Update()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(20, 0));
+    }
+}
