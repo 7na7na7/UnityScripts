4dd0af4c0f21869fbd9ae621ebaddd87ec089808
SexyM <hmg6435573@gmail.com>
2019-07-27 16:58:53 +0900

    Update 배열.cs

--- 배열.cs
+++ 배열.cs
@@ -16,5 +16,9 @@ public class 배열 : MonoBehaviour
     }
     //위는 스크립트 내에서 배열에 할당하는 방식,
     public int[] array; //그러나 이렇게 public 으로 배열을 할당받을 수도 있다.
-    
+    for (i = 0; i < array.Length; i++)
+    {
+        array[i] = 0;
+    }
+    //.Length를 사용하면 배열의 길이를 구할 수 있다. 이것으로 반복문을 돌릴 수 있다.
 }
