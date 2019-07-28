8614cd4d6dc6fd06a144d48215f01170259de886
SexyM <hmg6435573@gmail.com>
2019-07-28 20:19:44 +0900

    부모 설정!
    생성할 때 바로 부모를 설정해 줄 수도 있고, 생성된 오브젝트를 특성 오브젝트의 자식으로 만들어 줄 수도 있다!

---
+++ 부모설정.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 부모설정 : MonoBehaviour
+{
+    public Transform parent;
+    public GameObject child;
+    
+    void Start()
+    {
+        save save = GameObject.Find("save").GetComponent<save>(); //참조
+        GameObject Object = Instantiate(child, parent);//lockimg생성과 동시에 parent를 부모로 설정
+        Object.transform.position = new Vector3(parent.transform.position.x, parent.transform.position.y, parent.transform.position.z); //부모와 포지션을 같게 설정
+        child.transform.SetParent(parent.transform);//child의 부모를 parent로 설정
+    }
+}
