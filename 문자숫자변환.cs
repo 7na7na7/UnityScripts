bd218a0676235f6bbb38847bcf89cb9e86c2e7af
SexyM <hmg6435573@gmail.com>
2019-08-27 17:22:04 +0900

    Create 문자숫자변환.cs

---
+++ 문자숫자변환.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 문자숫자변환: MonoBehaviour
+{
+    //문자 -> 숫자 
+    private int two = int.Parse("1")+int.Parse("1");
+
+    //숫자 -> 문자      
+    .ToString()
+    void Start()
+    {
+        Debug.Log(two);
+        //결과는 2
+    }
+}
