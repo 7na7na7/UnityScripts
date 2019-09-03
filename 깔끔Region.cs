fb61c13da54aa5747a4fad788d444236771bc531
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-09-03 10:03:14 +0900

    9_3
    첫 개인노트북 커밋,
    region을 이용하면 코드를 짧게 간추릴 수 있고, 깔끔해진다!

---
+++ 깔끔Region.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 깔끔Region : MonoBehaviour
+{
+    //region을 이용해 싱글톤을 깔끔하게 표현해 보자!
+    #region Singleton
+    public static 깔끔Region instance;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this);
+            instance = this;
+        }
+    }
+    #endregion Singleton
+
+}
