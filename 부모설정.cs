eeb9c82af03d7598713981f48d223163c6854256
SexyM <hmg6435573@gmail.com>
2019-09-10 08:03:54 +0900

    Update 부모설정.cs

--- 부모설정.cs
+++ 부모설정.cs
@@ -9,6 +9,7 @@ public class 부모설정 : MonoBehaviour
     
     void Start()
     {
+        gameObject.transform.parent.GetComponent<찾고자하는 변수가 존재하는 스크립트명>().변수명
         save save = GameObject.Find("save").GetComponent<save>(); //참조
         GameObject Object = Instantiate(child, parent);//lockimg생성과 동시에 parent를 부모로 설정
         Object.transform.position = new Vector3(parent.transform.position.x, parent.transform.position.y, parent.transform.position.z); //부모와 포지션을 같게 설정
