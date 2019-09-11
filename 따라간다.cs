2b39dcbb9c8b865f66674d5fb19a98d92898bd52
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-09-12 00:22:54 +0900

    타겟따라가기, 해상도조정
    타겟 주위를 맴돌며 따라가게 할 수 있고(멋있다), 해상도를 코드를 사용하여 조정할 수 있다.

---
+++ 따라간다.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 따라간다 : MonoBehaviour
+{
+    public Transform target;
+    float moveSpeed;
+    float rotSpeed;
+ 
+    void Start(){
+        moveSpeed = Random.Range(5,20);
+        rotSpeed = Random.Range(5,10);
+    }
+    void Update(){
+        Vector3 vec = target.position - transform.position;
+        Quaternion q = Quaternion.LookRotation(vec);
+        Quaternion s = Quaternion.Slerp(transform.rotation, q, rotSpeed*Time.deltaTime);
+        transform.rotation = s;
+        transform.Translate(new Vector3(0,0,1) * moveSpeed * Time.deltaTime);
+    }
+}
