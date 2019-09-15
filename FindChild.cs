7f60f1e2a17f87d64157a4f43cd8c8c7e79c7341
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-09-16 08:58:01 +0900

    자식찾기
    저번에는 엄마를 찾아봤죠?
    이번에는 자식을 찾아보아요!

---
+++ FindChild.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindChild : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(transform.GetChild(0).name); //인덱스로 접근하는 방법
+        Debug.Log(transform.Find("panel").name); //오브젝트의 이름으로 접근하는 방법
+        GameObject child = transform.Find("panel").gameObject;
+        child.SetActive(false);
+    }
+}
