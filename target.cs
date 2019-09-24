abf83a924c25faf5f679ac5e8545b1471348aef8
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-09-24 19:33:03 +0900

    간단한따라가는코드

---
+++ target.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour
+{
+    public Transform tar;
+    private Vector3 dir;
+    public float speed = 5;
+    
+    void Update()
+    {
+        dir = tar.position - this.transform.position;
+        dir.Normalize();
+        transform.Translate(dir*speed*Time.deltaTime);
+    }
+}
