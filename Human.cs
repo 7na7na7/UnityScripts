6c2894bd16e81cb42c265aaec1968070f8ee6a02
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-09-27 22:42:32 +0900

    상속, 가상 함수, 오버라이드, 추상 클래스

---
+++ Human.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Human : MonoBehaviour //abstract를 사용하기 위해서 추상 클래스로 변환시킴
+{ 
+    //인간 클래스의 이름과 나이를 선언함
+    protected string humanName; //protected를 사용하면 상속받은 자식 클래스만 사용할 수 있게 됨
+    protected int humanAge;
+
+    protected virtual void Info() //virtual을 이용하여 가상 함수로 만듬
+    //가상 함수를 사용하면 override를 사용하여 재정의할 수 있게 된다.
+    {
+        Debug.Log("나는 인간입니다.");
+    }
+
+    abstract protected void Name(); //abstract를 이용하여 추상 함수로 만듬
+    //추상 함수를 사용하면 일단 함수를 정의해 놓고, 자식 클래스가 함수를 만들도록 함.
+    //Name 함수를 만들지 않을 경우, 오류가 발생함
+}
---
+++ Student.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Student : Human
+{
+    private string schoolName;
+    void Start()
+    {
+        schoolName = "대마고";
+        humanName = "민구";
+        humanAge = 17;
+        Info(); //"나는 인간입니다" 출력
+    }
+
+    protected override void Info()
+    {
+        base.Info(); //base는 부모 클래스, Human을 가리킴. 즉, Human.Info(); 인 셈이다.
+        Debug.Log("나는 학생입니다.");
+        //"나는 인간입니다", "나는 학생입니다" 두 가지가 모두 출력됨
+    }
+
+    protected override void Name() //추상 함수를 안만들면 오류가 나므로 반드시 만들어야 함
+    {
+        Debug.Log(humanName);
+    }
+}
