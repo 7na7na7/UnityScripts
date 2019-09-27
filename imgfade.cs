69fe9229294c62b0613ad8f0731440aa248e983e
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-09-27 11:55:30 +0900

    이미지투명화
    이미지도 할뚜이따할뚜이따!

---
+++ imgfade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class imgfade : MonoBehaviour
+{
+    private GameObject obj;
+    public float speed;
+    private Image img;
+    private Color fadecolor;
+
+    private void Start()
+    {
+        obj = this.gameObject;
+        img = GetComponent<Image>();
+        StartCoroutine(fade());
+    }
+
+    IEnumerator fade()
+    {
+        while (true)
+        {
+            fadecolor = img.color;
+            fadecolor.a -= 0.1f;
+            yield return new WaitForSeconds(speed);
+            img.color = fadecolor;
+            if(img.color.a<=0)
+                Destroy(obj);
+        }
+    }
+}
