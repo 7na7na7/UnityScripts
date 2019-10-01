e0444d1662fc7a219ef516ce84080dd4401604df
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-10-01 23:14:05 +0900

    10_1 체력바
    몬스터 위치가 담긴 리스트와 체력바 리스트를 선언하고,  캔버스에 스크립트를 넣으면 스타트에서 Add로 리스트에 몬스터의 태그를 가진 모든 오브젝트들을 검색하여 리스트에 넣는다. 그리고 얻어온 몬스터 위치 리스트를 이용하여 업데이트로 모든 체력바를 각각에 맞는 몬스터에 맞춰 움직이도록 한다.

---
+++ HpBarScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpBarScript : MonoBehaviour
+{
+    [SerializeField] private GameObject m_goPrefab = null; //SerializeField를 사용했는데 그냥 public써도된다.
+    List<Transform> m_objectList=new List<Transform>(); //몬스터 위치가 담긴 리스트 선언
+    List<GameObject> m_hpBarList= new List<GameObject>(); //Hp바 리스트 선언
+
+    private Camera m_cam = null;
+    void Start()
+    {
+        m_cam=Camera.main; //Camera.main이란 MainCamera로 태그된 카메라를 말한다.
+
+        GameObject[] t_objects = GameObject.FindGameObjectsWithTag("Player");
+        for (int i = 0; i < t_objects.Length; i++)
+        {
+            
+            m_objectList.Add(t_objects[i].transform);
+            //몬스터 위치에 HP바 프리팹 생성
+            GameObject t_hpbar = Instantiate(m_goPrefab, t_objects[i].transform.position, Quaternion.identity,transform);
+            //생성된 객체는 hp바 리스트에 추가
+            m_hpBarList.Add(t_hpbar);
+        }
+    }
+    
+    void Update()
+    {
+        for (int i = 0; i < m_objectList.Count; i++)
+        {
+            //hp바가 몬스터 머리 위로 따라다니게 설정
+            m_hpBarList[i].transform.position = 
+                m_cam.WorldToScreenPoint(m_objectList[i].position+new Vector3(0,1.15f,0));
+        }
+    }
+}
