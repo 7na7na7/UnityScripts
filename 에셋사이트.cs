1893bb9adaa02997edec8a1e7ec3799f0a4dc6d0
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-10-04 02:29:18 +0900

    에셋을 찾고 싶나요?
    겁나많네;;

---
+++ 에셋사이트.cs
@@ -0,0 +1,296 @@
+﻿유니티 에셋 스토어 : https://assetstore.unity.com
+OpenGameArt : https://opengameart.org/
+Kenney : https://www.kenney.nl/
+itch.io : https://itch.io/game-assets/free
+https://www.spriters-resource.com/
+
+--------- 2D 스프라이트 ----------
+Sprite Database
+http://sdb.drshnaps.com/
+
+Userpedia
+http://wiki.userpedia.net/Sprites
+
+Free Game Assets
+http://www.gameart2d.com/freebies.html
+
+Scroll Boss
+http://scrollboss.illmosis.net/mainmenu.php
+
+OpenGameArt
+https://opengameart.org/content/2d-complete-characters
+
+Super Sprite
+http://erantzo.awardspace.com/links.html
+
+Game Sprite Archives
+http://www.gsarchives.net/index2.php
+
+The Shyguy Kingdom
+http://tsgk.captainn.net/ 
+
+The Video Game Museum
+http://www.vgmuseum.com/
+
+Video Game Map
+http://vgmaps.com/
+
+Video Game Sprites
+http://www.videogamesprites.net/
+
+Pixel Art at pixeljoint.com
+http://www.pixeljoint.com/default.asp
+
+The Sprite Cemetery
+http://www.panelmonkey.org/
+
+Sprite Dungeon
+http://spritedungeon.tripod.com/index.html
+
+MegaMan Sprites
+http://eod.notart.org/sprites/
+
+opengamegraphics
+https://opengamegraphics.com/
+
+free stuff
+http://www.dumbmanex.com/bynd_freestuff.html
+
+glitch
+http://www.glitchthegame.com/public-domain-game-art/
+
+Kenney
+http://kenney.nl/
+
+untamed.wild-refuge
+http://untamed.wild-refuge.net/rmxpresources.php?characters
+
+toptal
+https://www.toptal.com/designers/subtlepatterns/
+
+lostgarden
+http://www.lostgarden.com/2008/07/directory-of-posts.html
+
+sharecg
+https://www.sharecg.com/store/index.php
+
+reiner's tilesets
+http://www.reinerstilesets.de/
+
+dribbble
+https://dribbble.com/
+
+----------- 사운드/효과음 ------------
+Free sound
+http://freesound.org/
+
+Soundclip
+http://www.soundclip.co.kr/bbs/board.php?bo_table=soriyo
+
+Zreo 
+http://www.zreomusic.com/download
+
+Audiojungle
+https://audiojungle.net/
+
+audiomicro
+http://www.audiomicro.com/sound-effects
+
+flashkit
+http://www.flashkit.com/soundfx
+
+youtube lib
+https://www.youtube.com/audiolibrary/soundeffects
+
+letscc
+http://www.letscc.net/
+
+ccmixter
+http://www.ccmixter.org/
+
+freemusicarchive
+http://www.freemusicarchive.org/
+
+jamendo
+https://www.jamendo.com/
+
+freesoundeffects
+http://www.freesoundeffects.com/
+
+animal memozee
+http://animal.memozee.com/animal/SOUND/
+
+free-loops
+http://free-loops.com/8619-nasty-basslines-140.html
+
+soundbible
+http://soundbible.com/2190-Front-Desk-Bell.html
+
+ushk
+http://ushk.tripod.com/sounds/sounds.html
+
+mediacollege
+http://www.mediacollege.com/downloads/sound-effects/
+
+3dmmstudio
+http://www.3dmmstudio.co.uk/sounds.php
+
+dramaticpublishing
+http://www.dramaticpublishing.com/free-sound-effects
+
+freesfx
+http://www.freesfx.co.uk/soundeffectcats.html
+
+soundjay
+https://www.soundjay.com/
+
+creativesounddesign
+http://creativesounddesign.com/the-recordist-free-sound-effects/
+
+members
+http://www.members.tripod.com/~buggerluggs/ie/wav-dir184.htm
+
+pachd
+http://www.pachd.com/sounds.html
+
+get-sounds
+http://www.get-sounds.com/
+
+nature-downloads
+http://nature-downloads.naturesounds.ca/
+
+pacdv
+http://www.pacdv.com/sounds/
+
+sound-ideas
+https://www.sound-ideas.com/Default.aspx
+
+talkingwav //Link들 있음
+http://www.talkingwav.com/
+
+wav-sounds
+http://www.wav-sounds.com/
+
+moviesoundclips
+http://www.moviesoundclips.net/sound-effects.php
+
+stoutman
+http://sounds.stoutman.com/sounds.php?Category=Animals
+
+boogiejack
+http://www.boogiejack.com/sound_effects.html
+
+findsounds
+http://www.findsounds.com/
+
+soundsnap
+https://www.soundsnap.com/
+
+soundsplanet
+http://soundsplanet.com/
+
+orlamusic
+https://orlamusic.sourceaudio.com/#!albums
+
+avangs
+http://avangs.info/sound_db
+
+soundboard
+http://www.soundboard.com/
+
+http://www.a1freesoundeffects.com/
+
+---------- 3D 모델 ----------
+Archive
+https://archive3d.net/
+
+3D Export
+https://3dexport.com/free-3d-models
+
+3dxtras
+http://www.3dxtras.com/
+
+3dmodelfree
+http://www.3dmodelfree.com/
+
+3D content central
+http://www.3dcontentcentral.com/default.aspx
+
+Free 3D
+https://free3d.com/
+
+Turbo Squid
+https://www.turbosquid.com/Search/?KEYWORD=Free
+
+Artist 3D
+http://artist-3d.com/
+
+CGI
+http://cg-india.com/cgblog_3dmodels.html
+
+Google 3D warehouse
+https://3dwarehouse.sketchup.com/index.html
+
+---------- 컬러픽 사이트 ------------
+https://color.adobe.com/ko/
+
+http://www.checkmycolours.com/
+
+http://www.color-hex.com/
+
+http://colorhunter.com/
+
+http://labs.tineye.com/multicolr/
+
+http://www.spycolor.com/
+
+http://www.colorexplorer.com/
+
+http://paletton.com/
+
+http://www.colr.org/
+
+http://www.colourgrab.com/
+
+http://colrd.com/
+
+---------- 텍스쳐 ----------
+http://texturemate.com/
+
+https://www.textures.com/
+
+---------- 이미지 -----------
+https://pixabay.com/
+
+https://www.freepik.com/
+
+https://www.pexels.com/
+
+https://www.deviantart.com/
+
+---------- 오픈소스 인디게임 사이트 -----------
+https://itch.io/
+
+----------- 아이콘  ----------
+http://www.graphicsfuel.com/2016/01/bank-and-money-icons/
+
+https://dribbble.com/shots/895385-Hexicons
+
+--------- 버튼 만들기 ---------
+Da Button Factory
+http://dabuttonfactory.com/
+
+-------- 텍스트 만들기 ---------
+Flamingtext
+http://www.flamingtext.com/logo/Design-Glow
+
+---------- 폰트 -----------
+1001 FreeFont
+http://www.1001freefonts.com/
+Naver FreeFont
+http://software.naver.com/software/fontList.nhn?categoryId=I0000000
+무료 한글 글꼴 사용범위 정리
+https://docs.google.com/spreadsheets/d/1AIJwVxjNWlBjeTDapfsT29mMOGa9DyNhIeEG_lKhluw/edit#gid=0
+
+-------- 포토샵 브러쉬 --------
+https://brushez.com/
