d640f2a14b7de3370e6479e77aab4f22d9729ae3
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-10-08 22:12:54 +0900

    PUN2멀티플레이어게임
    Online_Shooting의 소스코드이다.

---
+++ BulletScript.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using Photon.Realtime;
+
+public class BulletScript : MonoBehaviourPunCallbacks
+{
+    public float speed;
+    public PhotonView PV;
+    private int dir;
+
+    void Start()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime * dir);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+            PV.RPC("DestroyRPC", RpcTarget.AllBuffered);
+        if (!PV.IsMine && other.CompareTag("Player") && other.GetComponent<PhotonView>().IsMine) //느린쪽에 맞춰 히트판정, 맞는 사람 기준으로 안억울하게
+        {
+            other.GetComponent<PlayerScript>().Hit();
+            PV.RPC("DestroyRPC",RpcTarget.AllBuffered);
+        }
+    }
+
+    [PunRPC]
+    void DirRPC(int dir)
+    {
+        this.dir = dir;
+    }
+
+    [PunRPC]
+    void DestroyRPC()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ NetworkManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using Random = System.Random;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+  public InputField NickNameInput;
+  public GameObject DisconnectPanel;
+  public GameObject RespawnPanel;
+
+  void Awake()
+  {
+    Screen.SetResolution(960, 540, false);
+    PhotonNetwork.SendRate = 60;
+    PhotonNetwork.SerializationRate = 30;
+  }
+
+  public void Connect()
+  {
+    PhotonNetwork.ConnectUsingSettings(); //
+  }
+
+public override void OnConnectedToMaster()
+  {
+    PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+    PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 6 }, null);
+  }
+
+  public override void OnJoinedRoom()
+  {
+    DisconnectPanel.SetActive(false);
+    StartCoroutine(DestroyBullet());
+    Spawn();
+  }
+  IEnumerator DestroyBullet() //리스폰할 때 모든 총알 제거
+  {
+    yield return new WaitForSeconds(0.2f);
+    foreach (GameObject GO in GameObject.FindGameObjectsWithTag("Bullet")) GO.GetComponent<PhotonView>().RPC("DestroyRPC", RpcTarget.All);
+  }
+  public void Spawn()
+  {
+    PhotonNetwork.Instantiate("Player", new Vector3(UnityEngine.Random.Range(7,16),4,0), Quaternion.identity);
+    RespawnPanel.SetActive(false);
+  }
+  void Update() { if (Input.GetKeyDown(KeyCode.Escape) && PhotonNetwork.IsConnected) PhotonNetwork.Disconnect(); }
+  public override void OnDisconnected(DisconnectCause cause)
+  {
+    DisconnectPanel.SetActive(true);
+    RespawnPanel.SetActive(false);
+  }
+}
