ed58c217d185491b778ea3967cf483e165909212
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-10-26 11:42:02 +0900

    10_26_2차원배열을인스펙터에!

---
+++ 2차원배열 인스펙터.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 2차원배열인스펙터 : MonoBehaviour
+{
+[System.Serializable]  //  MonoBehaviour가 아닌 클래스에 대해 Inspector에 나타내기.
+public class MapArray
+{
+    public GameObject[] data;
+}
+
+public class MyScript : MonoBehaviour
+{
+    public MapArray[] map;
+}
+//이런 식으로 2차원 게임오브벡트를 인스펙터에 사용할 표시할 수 있다.
+}
