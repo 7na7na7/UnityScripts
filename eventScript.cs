42070ec25cde86b791444884917848272c72e918
SexyM <48942066+SexyMg@users.noreply.github.com>
2019-11-05 22:09:15 +0900

    이벤트, 델리게이트
    한 클래스 내의 함수를 한꺼번에 관리할 수 있는 델리게이트, 그리고 public static event 델리게이트함수이름 이벤트객체이름; 로 이벤트 객체를 만들어 다른 클래스에서도 사용할 수 있다.

---
+++ eventScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventScript : MonoBehaviour
+{ 
+    void Start()
+    {
+        deleScript.OnStart += Abc; 
+        //deleScript의 이벤트 객체 Onstart에 Abc함수를 넣음, 서로 다른 클래스지만 이렇게 묶을 수 있음
+   }
+
+    public void Abc(int value)
+    {
+        print(value + "값이 증가했습니다.");
+    }
+}
