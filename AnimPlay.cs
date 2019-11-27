d1cc41f67a153bd69462579ad89bf3e2bfef9fab
SexyM <hmg6435573@gmail.com>
2019-11-27 19:31:12 +0900

    애니메이션 상태쳌하기

--- AnimPlay.cs
+++ AnimPlay.cs
@@ -13,13 +13,41 @@ public class AnimPlay : MonoBehaviour
     {
         StartCoroutine(animtest());
     }
-
-    IEnumerator animtest()
+    IEnumerator animtest() 
     {
         anim.Play("run");
         yield return new WaitForSeconds(3f);
         anim.Play("walk");
         yield return new WaitForSeconds(3f);
         anim.Play("JumpAnim");
+    }
+    
+    
+    
+    public Animator animator;
+    private void Start()
+    {
+        StartCoroutine(CheckAnimationState());
+    }
+   
+    IEnumerator CheckAnimationState() //현재 애니메이션 상태 체크
+    {
+
+        while (!animator.GetCurrentAnimatorStateInfo(0)
+            .IsName("원하는 애니메이션 이름")) 
+        { 
+            //전환 중일 때 실행되는 부분
+            yield return null;
+        }
+
+        while (animator.GetCurrentAnimatorStateInfo(0)
+                   .normalizedTime < 1) //normalizedTime은 0에서 1까지의 값, 1보다 작으면 실행 중
+        {
+            //애니메이션 재생 중 실행되는 부분
+            yield return null;
+        }
+	
+        //애니메이션 완료 후 실행되는 부분
+
     }
 }
