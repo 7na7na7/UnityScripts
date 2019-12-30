bed6d96b3bae4731d47d05558b0802c246bd577e
SexyM <hmg6435573@gmail.com>
2019-12-30 09:27:36 +0900

    플레이어데미지깜박깜박

---
+++ 깜박깜박.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 깜박깜박 : MonoBehaviour
+{
+    public float invisibleTime;
+    IEnumerator invisible()
+    {
+        Color color;
+        SpriteRenderer sprite= GetComponent<SpriteRenderer>();//스프라이트로 함
+        color.r = 255;
+        color.g = 255;
+        color.b = 255;
+        color.a = 0.5f;
+        sprite.color = color;
+        yield return new WaitForSeconds(invisibleTime);
+        color.a = 1f;
+        sprite.color = color;
+        yield return new WaitForSeconds(invisibleTime);
+        color.a = 0.5f;
+        sprite.color = color;
+        yield return new WaitForSeconds(invisibleTime);
+        color.a = 1f;
+        sprite.color = color;
+        yield return new WaitForSeconds(invisibleTime);
+        color.a = 0.5f;
+        sprite.color = color;
+        yield return new WaitForSeconds(invisibleTime);
+        color.a = 1f;
+        sprite.color = color;
+        yield return new WaitForSeconds(invisibleTime);
+    }
+}
