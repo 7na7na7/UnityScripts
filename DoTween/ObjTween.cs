9002309bf523d4cdd6062fb6f930c244fdc80d8a
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-01-10 18:18:32 +0900

    두트윈 DoTween! 움직일때 개편하다!

---
+++ DoTween/ObjTween.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening; //에셋 불러옴
+
+public class ObjTween : MonoBehaviour
+{
+    public Transform pos;
+    void Start()
+    {
+        /*
+        transform.DOMove(Vector3 to, float duration, bool snapping);
+        to : 이동하려는 최종 좌표값
+        duration : 몇 초 동안 이동할 것있가.
+        snapping : true이면 이동할때 정수단위로 이동한다.
+        */
+        
+        //5초 동안 위로 이동, 1초 후 실행, 통통튀는움직임
+        transform.DOMove(Vector2.up, 3).SetDelay(1).SetEase(Ease.InBounce);
+
+        //5초 동안 스케일을 20,20으로 바꿈, 1초 후 실행 
+        transform.DOScale(Vector2.one*20, 5).SetDelay(1);
+
+        //5초 동안 오른쪽으로 60도 회전, 1초 후 실행
+        transform.DORotate(Vector3.back*60, 5).SetDelay(1);
+
+        //5초 동안 색을 빨강색으로 바꿈, 1초 후 실행
+        SpriteRenderer spr = GetComponent<SpriteRenderer>();
+        spr.DOColor(Color.red, 5).SetDelay(1);
+    }
+}
---
+++ DoTween/UITween.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+//트위닝 에셋 사용
+public class UITween : MonoBehaviour
+{
+    void Start()
+    {
+        //1초 동안 RectTransform의 Y값을 -300으로 만듬, Elastic효과로 개멋지게
+        RectTransform rt = GetComponent<RectTransform>();
+        rt.DOAnchorPosY(-300,1).SetEase(Ease.OutElastic);
+
+        //2초 동안 텍스트를 스크램블 효과를 주며 바꿈, 1초 후 실행
+        Text text = GetComponent<Text>();
+        text.DOText("Into the Unknown~", 2, true, ScrambleMode.None).SetDelay(1);
+    }
+}
