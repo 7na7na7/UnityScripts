6996acd550441694d383cd58bf6012cc4526a273
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-01-20 23:19:07 +0900

    스프라이트 사이즈 구하는법!

---
+++ 스프라이트사이즈.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 스프라이트사이즈 : MonoBehaviour
+{
+    private void Start()
+    {
+        print(GetSpriteSize(gameObject));
+    }
+
+    public Vector3 GetSpriteSize(GameObject _target)
+    {
+        Vector3 worldSize = Vector3.zero;
+        if(_target.GetComponent<SpriteRenderer>())
+        {
+            Vector2 spriteSize = _target.GetComponent<SpriteRenderer>().sprite.rect.size;
+            Vector2 localSpriteSize = spriteSize / _target.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+            worldSize = localSpriteSize;
+            worldSize.x *= _target.transform.lossyScale.x;
+            worldSize.y *= _target.transform.lossyScale.y;
+        }
+        else
+        {
+            Debug.Log ("SpriteRenderer Null");
+        }
+        return worldSize;
+    }
+}
