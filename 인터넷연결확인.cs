018029f449c0b59b07a03573018b958f14f2e1c3
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-02-04 22:19:19 +0900

    인터넷에 연결되어 있는지 확인할 수 있어!

---
+++ 인터넷연결확인.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 인터넷연결확인 : MonoBehaviour
+{
+    void Start()
+    {
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            print("인터넷에 연결되지 않음!");
+        }
+        else if (Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork)
+        {
+            print("데이터로 연결됨!");
+        }
+        else
+        {
+            print("와이파이로 연결됨!");
+        }
+    }
+}
