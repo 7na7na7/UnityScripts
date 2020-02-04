2a94e469c5a0fa6d3d9ab36ff6b68df6594638b2
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-02-04 22:23:49 +0900

    원하는 URL의 웹페이지를 열 수 있어!

---
+++ 웹페이지열기.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 인터넷연결확인 : MonoBehaviour
+{
+    void Start()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=KA3KUaeyDbw");
+    }
+}
