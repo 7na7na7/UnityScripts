bbcb9c079d6b76db8f167c42a9e5050c38ec208a
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-02-05 00:55:11 +0900

    포인터가 올려져있는지, 아닌지 판단가능. UI에만 되는지는 잘

---
+++ 포인터가올려져있는가.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class 포인터가올려져있는가 : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    private bool isBtnDown = false;
+ 
+    private void Update()
+    {
+        if (isBtnDown) {
+            print("A"); 
+            GetComponent<Text>().color = Color.black;
+        }
+        else
+        {
+            print("B");
+            GetComponent<Text>().color = new Color(200, 200, 200);
+        }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isBtnDown = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isBtnDown = false;
+    }
+}
