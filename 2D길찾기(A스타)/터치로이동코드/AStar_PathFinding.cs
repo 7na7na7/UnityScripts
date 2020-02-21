8729f0a976e44ae0797f089b64c5f678686c4a40
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-02-21 19:00:04 +0900

    A스타길찾기

---
+++ 2D길찾기(A스타)/터치로이동코드/AStar_PathFinding.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening; //에셋 불러옴
+
+[System.Serializable]
+public class Node
+{
+    public Node(bool _isWall, int _x, int _y) { isWall = _isWall; x = _x; y = _y; }
+
+    public bool isWall;
+    public Node ParentNode;
+
+    // G : 시작으로부터 이동했던 거리, H : |가로|+|세로| 장애물 무시하여 목표까지의 거리, F : G + H
+    public int x, y, G, H;
+    public int F { get { return G + H; } }
+}
+
+
+public class AStar_PathFinding : MonoBehaviour
+{
+    public GameObject Charactor;
+    public Vector2Int bottomLeft, topRight, startPos, targetPos;
+    public List<Node> FinalNodeList;
+    public bool allowDiagonal, dontCrossCorner;
+
+    int sizeX, sizeY;
+    Node[,] NodeArray;
+    Node StartNode, TargetNode, CurNode;
+    List<Node> OpenList, ClosedList;
+
+
+    public Vector3 dir = Vector3.zero;
+    private bool canGo = false;
+    public void PathFinding(Vector2Int _bottomLeft, Vector2Int _topRight, Vector2Int _startPos, Vector2Int _targetPos)
+    {
+        bottomLeft = _bottomLeft;
+        topRight = _topRight;
+        startPos = _startPos;
+        targetPos = _targetPos;
+        
+        // NodeArray의 크기 정해주고, isWall, x, y 대입
+        sizeX = topRight.x - bottomLeft.x + 1;
+        sizeY = topRight.y - bottomLeft.y + 1;
+        NodeArray = new Node[sizeX, sizeY];
+
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                bool isWall = false;
+                foreach (Collider2D col in Physics2D.OverlapCircleAll(new Vector2(i + bottomLeft.x, j + bottomLeft.y), 0.4f))
+                    if (col.gameObject.layer == LayerMask.NameToLayer("Wall")) isWall = true;
+
+                NodeArray[i, j] = new Node(isWall, i + bottomLeft.x, j + bottomLeft.y);
+            }
+        }
+
+        // 시작과 끝 노드, 열린리스트와 닫힌리스트, 마지막리스트 초기화
+        StartNode = NodeArray[startPos.x - bottomLeft.x, startPos.y - bottomLeft.y];
+        TargetNode = NodeArray[targetPos.x - bottomLeft.x, targetPos.y - bottomLeft.y];
+
+        OpenList = new List<Node>() { StartNode };
+        ClosedList = new List<Node>();
+        FinalNodeList = new List<Node>();
+
+        
+        while (OpenList.Count > 0)
+        {
+            // 열린리스트 중 가장 F가 작고 F가 같다면 H가 작은 걸 현재노드로 하고 열린리스트에서 닫힌리스트로 옮기기
+            CurNode = OpenList[0];
+            for (int i = 1; i < OpenList.Count; i++)
+                if (OpenList[i].F <= CurNode.F && OpenList[i].H < CurNode.H) CurNode = OpenList[i];
+            //이동비용이 가장 싼 것을 현재 노드(CurNode)로 함
+
+            OpenList.Remove(CurNode);
+            ClosedList.Add(CurNode);
+            
+            // 마지막
+            if (CurNode == TargetNode)
+            {
+                Node TargetCurNode = TargetNode;
+                while (TargetCurNode != StartNode)
+                {
+                    FinalNodeList.Add(TargetCurNode);
+                    TargetCurNode = TargetCurNode.ParentNode;
+                }
+                FinalNodeList.Add(StartNode);
+                FinalNodeList.Reverse();
+
+                //for (int i = 0; i < FinalNodeList.Count; i++) print(i + "번째는 " + FinalNodeList[i].x + ", " + FinalNodeList[i].y);
+                StopAllCoroutines();
+                StartCoroutine(Move(FinalNodeList.Count));
+                return;
+            }
+
+
+            // ↗↖↙↘
+            if (allowDiagonal)
+            {
+                OpenListAdd(CurNode.x + 1, CurNode.y + 1);
+                OpenListAdd(CurNode.x - 1, CurNode.y + 1);
+                OpenListAdd(CurNode.x - 1, CurNode.y - 1);
+                OpenListAdd(CurNode.x + 1, CurNode.y - 1);
+            }
+
+            // ↑ → ↓ ←
+            OpenListAdd(CurNode.x, CurNode.y + 1);
+            OpenListAdd(CurNode.x + 1, CurNode.y);
+            OpenListAdd(CurNode.x, CurNode.y - 1);
+            OpenListAdd(CurNode.x - 1, CurNode.y);
+        }
+    }
+
+    void OpenListAdd(int checkX, int checkY)
+    {
+        // 상하좌우 범위를 벗어나지 않고, 벽이 아니면서, 닫힌리스트에 없다면
+        if (checkX >= bottomLeft.x && checkX < topRight.x + 1 && checkY >= bottomLeft.y && checkY < topRight.y + 1 && !NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y].isWall && !ClosedList.Contains(NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y]))
+        {
+            // 대각선 허용시, 벽 사이로 통과 안됨(그러니까 내 위쪽과 오른쪽에 벽이 있으면 오른쪽 위로 대각선 이동 불가라는 뜻)
+            if (allowDiagonal) if (NodeArray[CurNode.x - bottomLeft.x, checkY - bottomLeft.y].isWall && NodeArray[checkX - bottomLeft.x, CurNode.y - bottomLeft.y].isWall) return;
+
+            // 코너를 가로질러 가지 않을시, 이동 중에 수직수평 장애물이 있으면 안됨
+            if (dontCrossCorner) if (NodeArray[CurNode.x - bottomLeft.x, checkY - bottomLeft.y].isWall || NodeArray[checkX - bottomLeft.x, CurNode.y - bottomLeft.y].isWall) return;
+
+            
+            // 이웃노드에 넣고, 직선은 10, 대각선은 14비용
+            Node NeighborNode = NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y];
+            int MoveCost = CurNode.G + (CurNode.x - checkX == 0 || CurNode.y - checkY == 0 ? 10 : 14);
+
+
+            // 이동비용이 이웃노드G보다 작거나(그래야 더 빠른길이니까) 또는 열린리스트에 이웃노드가 없다면(새로운 이웃노드추가해야지) G, H, ParentNode를 설정 후 열린리스트에 추가
+            if (MoveCost < NeighborNode.G || !OpenList.Contains(NeighborNode))
+            {
+                NeighborNode.G = MoveCost;
+                NeighborNode.H = (Mathf.Abs(NeighborNode.x - TargetNode.x) + Mathf.Abs(NeighborNode.y - TargetNode.y)) * 10;
+                NeighborNode.ParentNode = CurNode;
+
+                OpenList.Add(NeighborNode);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if(FinalNodeList.Count != 0) for (int i = 0; i < FinalNodeList.Count - 1; i++)
+                Gizmos.DrawLine(new Vector2(FinalNodeList[i].x, FinalNodeList[i].y), new Vector2(FinalNodeList[i + 1].x, FinalNodeList[i + 1].y));
+    }
+
+    IEnumerator Move(int count)
+    {
+        for (int i = 1; i < count; i++)
+        {
+            Vector2 v2 = new Vector2(FinalNodeList[i].x, FinalNodeList[i].y) - (Vector2) Charactor.transform.position;
+            dir = (Vector3) v2;
+            v2.Normalize();
+            yield return new WaitUntil(() => Vector3.Distance(new Vector3(FinalNodeList[i].x, FinalNodeList[i].y,0),Charactor.transform.position)<0.1f);
+        }
+        //Charactor.transform.position=new Vector3(Mathf.RoundToInt(Charactor.transform.position.x),Mathf.RoundToInt(Charactor.transform.position.y),0);
+        dir = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (dir != Vector3.zero)
+        {
+            if(Mathf.Abs(dir.x)-Mathf.Abs(dir.y)==0)
+                Charactor.transform.position += dir * Time.deltaTime *5*0.7f;
+            else
+                Charactor.transform.position += dir * Time.deltaTime *5f;
+        }
+    }
+}
---
+++ 2D길찾기(A스타)/터치로이동코드/MoveByClick.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByClick : MonoBehaviour
+{
+    private int Cx, Cy, Tx, Ty;
+    private AStar_PathFinding pf;
+    void Start()
+    {
+        pf = FindObjectOfType<AStar_PathFinding>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.transform.position=new Vector3(pf.Charactor.transform.position.x,pf.Charactor.transform.position.y,Camera.main.transform.position.z);
+       if(pf.dir==Vector3.zero)
+       {
+           //if(Input.GetTouch(0).phase==TouchPhase.Began)
+           if (Input.GetMouseButtonDown(1))
+           {
+               print("A");
+               Cx = Mathf.RoundToInt(pf.Charactor.transform.position.x);
+               Cy = Mathf.RoundToInt(pf.Charactor.transform.position.y);
+               Tx = Mathf.RoundToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition).x);
+               Ty = Mathf.RoundToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+
+               if (Tx >= Cx && Ty >= Cy) //오른쪽 위
+               {
+                   print("오른쪽위");
+                   pf.PathFinding(new Vector2Int(Cx - 10, Cy - 10), new Vector2Int(Tx + 10, Ty + 10),
+                       new Vector2Int(Cx, Cy), new Vector2Int(Tx, Ty));
+               }
+               else if (Tx <= Cx && Ty >= Cy) //왼쪽 위
+               {
+                   print("왼쪽위");
+                   pf.PathFinding(new Vector2Int(Tx - 10, Cy - 10), new Vector2Int(Cx + 10, Ty + 10),
+                       new Vector2Int(Cx, Cy), new Vector2Int(Tx, Ty));
+               }
+               else if (Tx >= Cx && Ty <= Cy) //오른쪽 아래
+               {
+                   print("오른쪽아래");
+                   pf.PathFinding(new Vector2Int(Cx - 10, Ty - 10), new Vector2Int(Tx + 10, Cy + 10),
+                       new Vector2Int(Cx, Cy), new Vector2Int(Tx, Ty));
+               }
+               else if (Tx <= Cx && Ty <= Cy) //왼쪽 아래
+               {
+                   print("왼쪽아래");
+                   pf.PathFinding(new Vector2Int(Tx - 10, Ty - 10), new Vector2Int(Cx + 10, Cy + 10),
+                       new Vector2Int(Cx, Cy), new Vector2Int(Tx, Ty));
+               }
+           }
+       }
+    }
+}
