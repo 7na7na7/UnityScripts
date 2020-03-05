db6866c428e2f37258a3e0e05706a914baa4cae1
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-03-05 14:06:36 +0900

    해상도고정하기

---
+++ 해상도고정_카메라에넣음.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 해상도고정_카메라에넣음 : MonoBehaviour
+{
+    void Start()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float scaleheight = ((float)Screen.width / Screen.height) / ((float)9 / 16); // (가로 / 세로)
+        float scalewidth = 1f / scaleheight;
+        if (scaleheight < 1)
+        {
+            rect.height = scaleheight;
+            rect.y = (1f - scaleheight) / 2f;
+        }
+        else
+        {
+            rect.width = scalewidth;
+            rect.x = (1f - scalewidth) / 2f;
+        }
+        camera.rect = rect;
+    }
+
+    void OnPreCull() => GL.Clear(true, true, Color.black);
+}
