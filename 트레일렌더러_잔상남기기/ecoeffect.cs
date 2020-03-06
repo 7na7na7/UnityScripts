60318ecb7c0533ae39918395d6a43eccf041422b
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-03-06 18:45:34 +0900

    트레일렌더러_잔상남기기

---
+++ 트레일렌더러_잔상남기기/ecoeffect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ecoeffect : MonoBehaviour
+{
+   public float timeBtwSpawns;
+   public float startTimeBtwSpawns;
+
+   public GameObject echo;
+
+   private void Update()
+   {
+      if (timeBtwSpawns <= 0)
+      {
+         GameObject ball=Instantiate(echo, transform.position, Quaternion.identity);
+         Destroy(ball,1f);
+         timeBtwSpawns = startTimeBtwSpawns;
+      }
+      else
+      {
+         timeBtwSpawns -= Time.deltaTime;
+      }
+   }
+}
---
+++ 트레일렌더러_잔상남기기/followcursor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class followcursor : MonoBehaviour
+{
+    //private TrailRenderer trail;
+    private void Start()
+    {
+        //trail = GetComponent<TrailRenderer>();
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //trail.startColor = Color.green;
+            //trail.endColor=Color.cyan;
+            //trail.endWidth = 3;
+        }
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = pos;
+    }
+}
