671b4acbe4147c02c5ffc3bd6d420d36ddec0abd
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-04-18 17:32:44 +0900

    해당방향 바라보기

---
+++ 이동관련/방향바라보기.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class 방향바라보기 : MonoBehaviour
+{
+    public GameObject target;
+    public float value = 0;
+    
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(0, 0, -getAngle(transform.position.x, transform.position.y, target.transform.position.x, target.transform.position.y)+value); 
+    }
+    private float getAngle(float x1, float y1, float x2, float y2) //Vector값을 넘겨받고 회전값을 넘겨줌
+    {
+        float dx = x2 - x1;
+        float dy = y2 - y1;
+
+        float rad = Mathf.Atan2(dx, dy);
+        float degree = rad * Mathf.Rad2Deg;
+        
+        return degree;
+    }
+}
