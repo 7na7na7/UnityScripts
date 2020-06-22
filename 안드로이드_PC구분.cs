73e324bd7fb956704daf378a0c472ff608628519
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-06-22 10:49:26 +0900

    안드로이드와 PC를 구분할수있다!

---
+++ 안드로이드_PC구분.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class 안드로이드_PC구분 : MonoBehaviour
+{
+    private void Start()
+    {
+#if(UNITY_ANDROID)
+        print("안드로이드");
+#else
+print("PC");
+#endif
+    }
+}
