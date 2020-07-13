758f3a653a80be0c30ddeb95bcc611d78adfb7de
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-07-13 10:49:54 +0900

    SendMassage로 함수호출하기
    첫번째인자가 함수이름, 두번째인자가 인수, 세번째인자가 옵션!
    성능저하가 조금 있을 수 있지만 크지 않다!

---
+++ SendMassage.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendMassage : MonoBehaviour {
+    void ApplyDamage(float damage) {
+        print(damage);
+    }
+    void Example() {
+        gameObject.SendMessage("ApplyDamage", 5.0F);
+    }
+}
