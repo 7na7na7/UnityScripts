e56790f5c22bfa93205a654522e82a1e22d3d228
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-07-13 19:46:11 +0900

    7_13_아래꺼는 거짓이야 속지마 이게진짜야;;

--- ResolutionSetting.cs
+++ ResolutionSetting.cs
@@ -4,29 +4,61 @@ using UnityEngine;
 
 public class ResolutionSetting : MonoBehaviour
 {
-   void Awake()
+   using System;
+   using System.Collections;
+   using System.Collections.Generic;
+   using UnityEngine;
+
+   public class ResolutionSetting : MonoBehaviour
    {
-      //남는부분을 Rect를 조정하려 래터박스로 채울 수 있다!
-      Camera camera = GetComponent<Camera>();
-      Rect rect = camera.rect;
-//      가로로 눕히는게임의경우
-//      float scaleWidth = ((float) Screen.width / Screen.height) / ((float) 9 / 16); //가로/세로
-//      float  scaleHeight= 1f / scaleWidth;
-      //세로로 하는게임의경우
-      float scaleHeight = ((float) Screen.width / Screen.height) / ((float) 9 / 16); //가로/세로
-      float scaleWidth = 1f / scaleHeight;
-      
+      private void Awake()
+      {
+         SetRes(false);
+      }
+
+    
+      public void SetRes(bool isPortrait)
+      {
+         //남는부분을 Rect를 조정하려 래터박스로 채울 수 있다!
+         Camera camera = GetComponent<Camera>();
+         Rect rect = camera.rect;
+
+         float scaleHeight, scaleWidth;
+         if (isPortrait)
+         {
+            //세로로 하는게임의경우
+            scaleHeight = ((float) Screen.width / Screen.height) / ((float) 9 / 16); //가로/세로
+            scaleWidth = 1f / scaleHeight;
+            if (scaleHeight < 1) //세로가 넓은경우
+            {
+               rect.height = scaleHeight;
+               rect.y = (1f - scaleHeight) / 2f;
+            }
+            else //가로가 넓은경우
+            {
+               rect.width = scaleWidth;
+               rect.x = (1f - scaleWidth) / 2f;
+            }
+         }
+         else
+         {
+            //      가로로 눕히는게임의경우
+            scaleWidth = ((float) Screen.width / Screen.height) / ((float) 16 / 9); //가로/세로
+            scaleHeight= 1f / scaleWidth;
+            if (scaleHeight < 1) //세로가 넓은경우
+            {
+               rect.width = scaleWidth;
+               rect.x = (1f - scaleWidth) / 2f;
+            }
+            else //가로가 넓은경우
+            {
+               rect.height = scaleHeight;
+               rect.y = (1f - scaleHeight) / 2f;
+            }
+         }
      
-      if (scaleHeight < 1) //세로가 넓은경우
-      {
-         rect.height = scaleHeight;
-         rect.y = (1f - scaleHeight) / 2f;
+         camera.rect = rect;
       }
-      else //가로가 넓은경우
-      {
-         rect.width = scaleWidth;
-         rect.x = (1f - scaleWidth) / 2f;
-      }
-      camera.rect = rect;
    }
+
 }
