4e2feb8da6e8b16db04f261b91f6819254a278b5
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-07-27 15:51:29 +0900

    bpm에 따라 정확하게 노트를 출력해보자!

---
+++ bpm노트출력.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bpm노트출력 : MonoBehaviour
+{
+    public int bpm = 0;
+    private double currentTime = 0d;
+    
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= 60d / bpm)
+        {
+            print("노트출력! "+Time.time);
+            currentTime -= 60d / bpm;
+        }
+    }
+}
