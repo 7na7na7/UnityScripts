d185aee1cb85aef10b56e1dc5b20e9653ff42f5c
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-07-27 23:14:27 +0900

    구글스프레드시트에 데이터 올리기
    개 쩐 다 !

---
+++ 구글스프레드시트 데이터올리기/SendToGoogle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SendToGoogle : MonoBehaviour
+{
+  public GameObject username, email, phone;
+
+  private string Name, Email, Phone;
+
+  [SerializeField] private string BASE_URL =
+      "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdsm55DNje3CHOVAAS-EUkKBD7MklCKNlwpsHksjoyiCBoWKg/formResponse";
+  public void Send()
+  {
+    Name = username.GetComponent<InputField>().text;
+   Email = email.GetComponent<InputField>().text;
+    Phone = phone.GetComponent<InputField>().text;
+
+    StartCoroutine(Post(Name, Email, Phone));
+  }
+
+  IEnumerator Post(string name, string email, string phone)
+  {
+      WWWForm form=new WWWForm();
+      form.AddField("entry.931206827",name);
+      form.AddField("entry.2027631050",email);
+      form.AddField("entry.1758129288",phone);
+      byte[] rawData = form.data;
+      WWW www=new WWW(BASE_URL,rawData);
+      yield return www;
+  }
+}
