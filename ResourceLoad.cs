dca320712dfbda5f4361dc5494de1860d76a501d
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-07-29 09:16:52 +0900

    리소스 편하게 로드하기 Resources.Load!

---
+++ ResourceLoad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceLoad : MonoBehaviour
+{
+    private void Start()
+    {
+       GameObject p=Instantiate(Resources.Load<GameObject>("characters/Player") , transform.position, Quaternion.identity);
+        // AssetResources폴더 안의 characters폴더에 있는 게임오브젝트 타입의 Player란 이름의 리소스 가져옴, Sprite형이나 AudioClip형도 가능!
+    }
+}
