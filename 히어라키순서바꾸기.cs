e74368d6cbf78663aae9b7b0b081ee79ea6aba2d
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-08-03 11:28:39 +0900

    히어라키순서 바꾸기!

---
+++ 히어라키순서바꾸기.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 히어라키순서바꾸기 : MonoBehaviour
+{
+    void Start()
+    {
+        transform.SetAsFirstSibling(); //히어라키에서 처음으로 순서 변경
+        transform.SetAsLastSibling(); //마지막으로 순서 변경
+        transform.SetSiblingIndex(4); //4번째에 위치하도록 순서 변경
+        print(transform.GetSiblingIndex()); //순서 반환
+    }
+}
