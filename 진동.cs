a26055a3e59eb81db7ea5829558305dba3b42540
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-08-18 23:18:05 +0900

    진동시키기! Handheld.Vibrate너무쉽다^^

---
+++ 진동.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 진동 : MonoBehaviour
+{
+    public void Vibration()
+    {
+        Handheld.Vibrate();
+    }
+}
