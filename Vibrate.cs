7370bdf0775ad9fd6669c2d29a937d1b4c4ab54b
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-08-19 12:07:10 +0900

    8_19_진동 시간도 조절가능하다!

---
+++ Vibrate.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class Vibrate : MonoBehaviour
+{
+    public static Vibrate instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    public static class Vibration
+    {
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    public static AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    public static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+    public static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+#else
+        public static AndroidJavaClass unityPlayer;
+        public static AndroidJavaObject currentActivity;
+        public static AndroidJavaObject vibrator;
+#endif
+
+        public static void Vibrate()
+        {
+            if (isAndroid())
+                vibrator.Call("vibrate");
+            else
+                Handheld.Vibrate();
+        }
+
+
+        public static void Vibrate(long milliseconds)
+        {
+            if (isAndroid())
+                vibrator.Call("vibrate", milliseconds);
+            else
+                Handheld.Vibrate();
+        }
+
+        public static void Vibrate(long[] pattern, int repeat)
+        {
+            if (isAndroid())
+                vibrator.Call("vibrate", pattern, repeat);
+            else
+                Handheld.Vibrate();
+        }
+
+        public static bool HasVibrator()
+        {
+            return isAndroid();
+        }
+
+        public static void Cancel()
+        {
+            if (isAndroid())
+                vibrator.Call("cancel");
+        }
+
+        private static bool isAndroid()
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+return true;
+#else
+            return false;
+#endif
+        }
+    }
+
+    public void Vibe(int range)
+    {
+        Vibration.Vibrate(range);
+    }
+}
