b726a8ff4c53dfe224916088d82c67cdd780e1b4
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-09-10 14:21:51 +0900

    가비지컬렉터작동-System.GC.Collect();
    System.GC.Collect();

---
+++ 가비지컬렉터작동.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 가비지컬렉터작동 : MonoBehaviour
+{
+    void Start()
+    {
+        System.GC.Collect();
+    }
+}
