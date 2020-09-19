5db6edf3711eefccb5b94e283cb41ddea415c21d
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-09-19 22:26:22 +0900

    Application.systemLanguage로 시스템언어 얻어와 번역하기

---
+++ language.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class language : MonoBehaviour
+{
+    public string Eng, Kor, Jap,Chin;
+    
+    void Start()
+    {
+        SystemLanguage lang = Application.systemLanguage;
+        switch (lang)
+        {
+            case SystemLanguage.Chinese:
+                GetComponent<Text>().text = Chin;
+                break;
+            case SystemLanguage.ChineseSimplified:
+                GetComponent<Text>().text = Chin;
+                break;
+            case SystemLanguage.ChineseTraditional:
+                GetComponent<Text>().text = Chin;
+                break;
+            case SystemLanguage.Korean:
+                GetComponent<Text>().text = Kor;
+                break;
+            case SystemLanguage.Japanese:
+                GetComponent<Text>().text = Jap;
+                break;
+            default:
+                GetComponent<Text>().text = Eng;
+                break;
+        }
+    }
+    
+}
