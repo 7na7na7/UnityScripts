9ea556d71b5556b396e498d0f7b3bea49fb76586
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-09-29 11:49:31 +0900

    아주아주아주간단한리지드바디무브

---
+++ SimpleTopdownMove.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTopdownMove : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    public float speed;
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rigid.velocity=new Vector2(Input.GetAxisRaw("Horizontal")*speed,Input.GetAxisRaw("Vertical")*speed);
+    }
+}
