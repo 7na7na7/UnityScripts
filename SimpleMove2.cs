cf79532f21f26bde19b8a8166eea74abce14fe75
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-02 02:13:27 +0900

    심플한 리지드바디이동_대각선도 동일한속도!

---
+++ SimpleMove2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove2 : MonoBehaviour
+{
+    private Vector2 moveDirection;
+     private Rigidbody2D rigid;
+        public float speed;
+        private void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+    
+        void Update()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            float moveX = Input.GetAxisRaw("Horizontal");
+            float moveY = Input.GetAxisRaw("Vertical");
+
+            moveDirection = new Vector2(moveX, moveY).normalized; //대각선 이동 정규화
+            rigid.velocity=new Vector2(moveDirection.x*speed,moveDirection.y*speed);
+        }
+}
