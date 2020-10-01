befda1ce4bef735ad2ae715c8dded481a725bf02
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-01 23:39:59 +0900

    심플한 리지드바디이동

---
+++ SimpleMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+     private Rigidbody2D rigid;
+        public float speed;
+        private void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+    
+        void Update()
+        {
+            rigid.velocity=new Vector2(Input.GetAxisRaw("Horizontal")*speed,Input.GetAxisRaw("Vertical")*speed);
+        }
+}
