855a32a58631d9c65393e39b01dd604b4c9420cb
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-26 10:43:38 +0900

    퍼센트 주면 그 확률대로 실행해주는 함수

---
+++ 퍼센트확률.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 퍼센트확률 : MonoBehaviour
+{
+    public void PercentReturn(int percent)
+    {
+        int r = Random.Range(0, 100);
+        bool[] bools=new bool[100];
+        for (int i = 0; i < 100; i++)
+        {
+            if (i < percent)
+                bools[i] = true;
+            else
+                bools[i] = false;
+        }
+        if (bools[r]==true)
+            print(percent+"퍼센트의 확률 성공!");
+        else
+            print(percent+"퍼센트의 확률 실패...");
+    }
+}
