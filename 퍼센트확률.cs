e36c7561121f0361bc9fb875ca5d32b18f05f71c
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-26 13:16:03 +0900

    진짜제대로된 간결한 킹갓 퍼센트확률

--- 퍼센트확률.cs
+++ 퍼센트확률.cs
@@ -6,16 +6,7 @@ public class 퍼센트확률 : MonoBehaviour
 {
     public void PercentReturn(int percent)
     {
-        int r = Random.Range(0, 100);
-        bool[] bools=new bool[100];
-        for (int i = 0; i < 100; i++)
-        {
-            if (i < percent)
-                bools[i] = true;
-            else
-                bools[i] = false;
-        }
-        if (bools[r]==true)
+        if(Random.Range(1,101)<=percent)
             print(percent+"퍼센트의 확률 성공!");
         else
             print(percent+"퍼센트의 확률 실패...");
