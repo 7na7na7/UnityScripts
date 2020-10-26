d74c21df3974da74e5f8ce5007ce7abe6ce1fe52
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-10-26 16:57:03 +0900

    CSV파일읽기! UTF-8인코딩으로 해줘야 한글이 안깨진다!

---
+++ CSVRead.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVRead : MonoBehaviour
+{
+    public int exp = 0;
+    void Start()
+    {
+        List<Dictionary<string, object>> data = CSVReader.Read("DataFile");
+
+        for (var i = 0; i < data.Count; i++)
+        {
+            print("Index "+(i).ToString()+" : "+data[i]["exp"]+data[i]["grade"] +data[i]["text"]);
+        }
+
+        exp = (int) data[0]["exp"];
+        print("exp : "+exp);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
