f3cecff6c4946b85934fcac420306c6b5f97911c
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-11-06 11:22:01 +0900

    눌린키 구하기, 지뢰찾기패키지

---
+++ GetKey.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetKey : MonoBehaviour
+{
+      void OnGUI()
+       {
+           print(Event.current.keyCode);
+           if (Event.current.Equals(Event.KeyboardEvent(KeyCode.Space.ToString())))
+           {
+               Debug.Log("스페이스 키가 눌렸습니다!");
+           }
+       }
+}
