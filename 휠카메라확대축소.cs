6c5e850b1563c0441315832bfbb981dfcce8f069
SexyM <48942066+SexyMg@users.noreply.github.com>
2020-12-14 20:17:38 +0900

    마우스 휠로 카메라를 확대하고 축소해보자!

---
+++ 휠카메라확대축소.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 휠카메라확대축소: MonoBehaviour
+{
+    public Camera cam;
+    public float max, min;
+    public float scrollSpeed;
+    private void Update() 
+    { 
+        float scroll = Input.GetAxis("Mouse ScrollWheel") * scrollSpeed*-1;
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize + scroll * scrollSpeed*Time.deltaTime, min, max);
+    }
+    
+}
