307cdef665d00d96e33678c3326c4c228063f5a0
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-01-19 21:57:59 +0900

    부모관계 해제하기

---
+++ 부모관계해제.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 부모관계해제 : MonoBehaviour
+{
+    void Start()
+    {
+        transform.parent = null; //너무쉽지?
+    }
+}
