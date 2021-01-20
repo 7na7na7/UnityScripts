ac447aec2e692949a9417eab383131268ffb3bf6
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-01-20 16:08:57 +0900

    함수들을 차례대로 실행하고 깔끔하게! 이벤트

---
+++ Event.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events; //필요
+    
+public class Event : MonoBehaviour
+{
+    public UnityEvent startEvent; //이벤트
+    void Start()
+    {
+        startEvent.Invoke(); //startEvent실행, 차례대로 함수들이 실행됨
+    }
+}
