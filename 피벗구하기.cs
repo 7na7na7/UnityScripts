af20a3586db4191345fcc215ae707a6438dde62e
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-01-20 16:52:14 +0900

    스프라이트의 피벗을 구해보자!

---
+++ 피벗구하기.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 피벗구하기 : MonoBehaviour
+{
+    private void Start()
+    {
+        print(transform.position+" "+GetComponent<SpriteRenderer>().bounds.center);
+    }
+}
