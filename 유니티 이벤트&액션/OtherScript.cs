bc1acc096d084cee0a376fb5d93ce5f75c63ae34
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-04-18 21:39:20 +0900

    슈정

--- 유니티 이벤트&액션/OtherScript.cs
+++ 유니티 이벤트&액션/OtherScript.cs
@@ -6,7 +6,7 @@ public class OtherScript : MonoBehaviour
 {
     void Start()
     {
-        FindObjectOfType<Event>().onInputSpace.AddListener(Space); //이렇게 추가 가능!
+        FindObjectOfType<Event_Action>().onInputSpace.AddListener(Space); //이렇게 추가 가능!
         //FindObjectOfType<Event>().onInputSpace.RemoveListener(hello); //이렇게 삭제 가능!
     }
 
