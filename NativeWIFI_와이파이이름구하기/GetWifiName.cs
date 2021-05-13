2b091af77a25ca9395f7e3a743fd1d5706d1f863
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-05-13 10:06:01 +0900

    NativeWIFI로 와이파이이름 구하기

---
+++ NativeWIFI_와이파이이름구하기/GetWifiName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using NativeWifi; //꼭 잇어야 실행댄다!
+
+
+public class GetWifiName : MonoBehaviour
+{
+    private void Start()
+    {
+        print("현재 연결된 와이파이 : "+GetAvailableWifi());
+    }
+
+    private string GetAvailableWifi()
+    {
+        WlanClient client = new WlanClient();
+        foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+        {
+            Wlan.WlanAvailableNetwork[] networks = wlanIface.GetAvailableNetworkList(0);
+            foreach (Wlan.WlanAvailableNetwork network in networks)
+            {
+                Wlan.Dot11Ssid ssid = network.dot11Ssid;
+                string networkname = Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
+                if (networkname != "")
+                {
+                    return networkname;
+                }
+            }
+        }
+
+        return "null";
+    }
+}
