baf59c71e77d0b4f4aa1305da989bba0873fde20
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-05-13 14:06:20 +0900

    DateTime.Now로 날짜랑 시간 구할수있다!

---
+++ 날짜시간.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 날짜시간 : MonoBehaviour
+{
+    void Update()
+    {
+        var date = DateTime.Now; 
+        print(string.Format("Today is {0}, it's {1:HH:mm} now.", date.DayOfWeek, date));
+    }
+}
