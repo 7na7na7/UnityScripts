502223fb26e2207ef5a3df5f58f99f30af2aa174
Mingu Hwang <48942066+SexyMg@users.noreply.github.com>
2021-09-30 10:54:24 +0900

    해상도고정! 나머지는 검은색으로...

---
+++ 해상도고정.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class 해상도고정 : MonoBehaviour
+{
+    public int width;
+    public int height;
+    private void Start()
+    {
+        SetResolution(); // 초기에 게임 해상도 고정
+    }
+
+    /* 해상도 설정하는 함수 */
+    public void SetResolution()
+    {
+        int setWidth = width; // 사용자 설정 너비
+        int setHeight = height; // 사용자 설정 높이
+
+        int deviceWidth = Screen.width; // 기기 너비 저장
+        int deviceHeight = Screen.height; // 기기 높이 저장
+
+        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true); // SetResolution 함수 제대로 사용하기
+
+        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight) // 기기의 해상도 비가 더 큰 경우
+        {
+            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight); // 새로운 너비
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // 새로운 Rect 적용
+        }
+        else // 게임의 해상도 비가 더 큰 경우
+        {
+            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // 새로운 높이
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
+        }
+    }
+}
