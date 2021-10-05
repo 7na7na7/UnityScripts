b4a8dccfc2792186218ae64b5c087bf9b54a5b9b
Mingu Hwang <48942066+SexyMg@users.noreply.github.com>
2021-10-05 12:46:45 +0900

    문자열보간 $"a={a}"

---
+++ 문자열보간.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 문자열보간 : MonoBehaviour
+{
+    void Start()
+    {
+        float f = 4.29f;
+        print($"my birthday is {f}");
+        print("my birthday is 4.29");
+    }
+}
