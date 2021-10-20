6115e0c0171d3c65bc71e5246ca4e9deec5e8f57
Mingu Hwang <48942066+SexyMg@users.noreply.github.com>
2021-10-20 11:26:25 +0900

    Create 코루틴종료_완전종료재시작.cs

---
+++ 코루틴종료_완전종료재시작.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 코루틴종료_완전종료재시작 : MonoBehaviour
+{
+    private Coroutine coroutine;
+    
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StopAndRestartCor();
+        }
+    }
+
+  void StopAndRestartCor()
+    {
+        if (coroutine != null)
+            StopCoroutine(coroutine);
+        coroutine = StartCoroutine(cor());
+    }
+
+    IEnumerator cor()
+    {
+        int i = 0;
+        while(true)
+        {
+            yield return new WaitForSeconds(1f);
+            print(i);
+            i++;
+        }
+    }
+}
