7b879d75ae346043aa49a586ae068b9efc630a85
Mingu Hwang <48942066+SexyMg@users.noreply.github.com>
2021-10-22 11:09:50 +0900

    스프레드시트 십게 json으로 바꾸기

---
+++ 스프레드시트_Json.cs
@@ -0,0 +1,9 @@
+﻿구글 스프레드시트 들어가기
+부가기능 선택
+부가기능 설치하기 선택
+Export Sheet Data 설치
+돌아와서 부가기능에서 Export Sheet Data선택
+Open Sidebar 눌러서 우측에 창띄움
+Export Format에서 XML 또는 JSON 선택
+하단에 Export 써있는 파란 버튼 클릭
+배열은 []이런식으로 알아서 설정해서 쓰자
