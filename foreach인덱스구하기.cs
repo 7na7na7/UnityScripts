b3f1f0469dc6291d507a1fa36b9416a890ba7260
Mingu Hwang <48942066+SexyMg@users.noreply.github.com>
2021-11-03 18:30:04 +0900

    foreach에서인덱스구해보자!
    GetSiblingIndex()으로 된다...!

---
+++ foreach인덱스구하기.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class foreach인덱스구하기 : MonoBehaviour
+{
+    public Transform[] trs;
+    private void Start()
+    {
+        foreach (var tr in trs)
+            print(tr.GetSiblingIndex());
+        //0 1 2 3 4 이렇게 출력해준다
+    }
+}
