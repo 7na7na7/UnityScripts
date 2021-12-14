fd11f3616a91a3aa602ec23e4f9df7a876a2f76c
nana <hmg6435573@gmail.com>
2021-12-14 13:17:40 +0900

    오브젝트가화면밖에있는지확인

---
+++ 화면밖인지확인.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class 화면밖인지확인 : MonoBehaviour
+{
+    public static 화면밖인지확인 instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+    
+    public bool CheckObjectIsInCamera(GameObject _target)
+    {
+        Camera selectedCamera = Camera.main;
+        Vector3 screenPoint = selectedCamera.WorldToViewportPoint(_target.transform.position);
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+
+        return onScreen;
+    }
+}
