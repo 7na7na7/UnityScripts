fa34b52e30250007397bbc5d9f1762260673e0a8
nana <hmg6435573@gmail.com>
2021-12-23 13:53:45 +0900

    Create 람다식할당.cs
    우와 신기하다!

---
+++ 람다식할당.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class 람다식할당 : MonoBehaviour
+{
+    public bool isDead = false;
+    public int hp = 10; 
+
+    public int Health => isDead ? 0 : hp;
+    //위의 =>는 이런 뜻이다!
+    /*
+    public int Health
+    {
+        get
+        {
+            return isDead ? hp : 0;
+        }
+    }
+    */
+
+    private void Start()
+    {
+        print(Health);
+    }
+}
